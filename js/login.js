33a6c1e5bcd33d3959cc8ef8dc9592791052a1db
lokeshmanapure <lokeshmanapure1@gmail.com>
2024-03-22 22:26:43 +0530

    Authentoication page done

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 
- import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+ import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
  const firebaseConfig = {
    apiKey: "AIzaSyAPsksv8RAmjEZI-VScGCoZ70dnG-HtQFA",
@@ -18,10 +18,30 @@
 
  const auth = getAuth(app);
     auth.languageCode = 'en';
- const provider = new GoogleAuthProvider();
 
+    
+
+const emaillogin = document.getElementById('emaillogin');
+emaillogin.addEventListener("click", function(){
+   
+   const email = document.getElementById('inputemail').value;
+   const password = document.getElementById('inputpassword').value;
+   signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+   
+});
+
+    
+ 
  const googlelogin = document.getElementById('signinwithgoogle');
  googlelogin.addEventListener("click", function(){
+   const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
     .then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -38,4 +58,26 @@
     const email = error.customData.email;
     const credential = GoogleAuthProvider.credentialFromError(error);
      });
- })
+ });
+
+
+ const facebooklogin = document.getElementById('signinwithfacebook');
+ facebooklogin.addEventListener("click", function(){
+   const provider = new FacebookAuthProvider();
+   signInWithPopup(auth, provider)
+   .then((result) => 
+   {
+   const user = result.user;
+   const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+
+   })
+
+   .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = FacebookAuthProvider.credentialFromError(error);
+   });
+
+});
